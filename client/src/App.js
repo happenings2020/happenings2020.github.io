d17712a2dfb1b8a1d5a165ea4ce455f97ad76ea9
Vipul Chodankar <vipul72c@gmail.com>
2020-03-03 21:33:06 +0530

    Added data and improved responsiveness

--- client/src/App.js
+++ client/src/App.js
@@ -2,42 +2,159 @@ import React, { Component } from 'react';
 import Slider from './components/NetflixSlider'
 import './App.scss'
 
-const movies = [
+const elite = [
   {
     id: 1,
     image: '/images/slide1.jpg',
     imageBg: '/images/slide1b.webp',
-    title: '1983'
+    title: 'The Crown'
   },
   {
     id: 2,
     image: '/images/slide2.jpg',
     imageBg: '/images/slide2b.webp',
-    title: 'Russian doll'
+    title: 'The Walking Dead'
   },
   {
     id: 3,
     image: '/images/slide3.jpg',
     imageBg: '/images/slide3b.webp',
-    title: 'The rain',
+    title: 'Rhythm + Flow',
   },
   {
     id: 4,
     image: '/images/slide4.jpg',
     imageBg: '/images/slide4b.webp',
-    title: 'Sex education'
+    title: 'Peaky Blinders'
   },
   {
     id: 5,
     image: '/images/slide5.jpg',
     imageBg: '/images/slide5b.webp',
-    title: 'Elite'
+    title: 'Sacred Games'
+  }
+];
+
+const premium = [
+  {
+    id: 1,
+    image: '/images/slide1.jpg',
+    imageBg: '/images/slide1b.webp',
+    title: 'Nobody Speak: Trials of the free passes'
+  },
+  {
+    id: 2,
+    image: '/images/slide2.jpg',
+    imageBg: '/images/slide2b.webp',
+    title: 'The Vikings'
+  },
+  {
+    id: 3,
+    image: '/images/slide3.jpg',
+    imageBg: '/images/slide3b.webp',
+    title: 'Westside',
+  },
+  {
+    id: 4,
+    image: '/images/slide4.jpg',
+    imageBg: '/images/slide4b.webp',
+    title: 'Altered Carbon'
+  },
+  {
+    id: 5,
+    image: '/images/slide5.jpg',
+    imageBg: '/images/slide5b.webp',
+    title: 'Money Heist'
   },
   {
     id: 6,
-    image: '/images/slide6.jpg',
-    imageBg: '/images/slide6b.webp',
-    title: 'Black mirror'
+    image: '/images/slide5.jpg',
+    imageBg: '/images/slide5b.webp',
+    title: 'Tug Of War'
+  }
+];
+
+const standard = [
+  {
+    id: 1,
+    image: '/images/slide1.jpg',
+    imageBg: '/images/slide1b.webp',
+    title: 'Pumping Iron'
+  },
+  {
+    id: 2,
+    image: '/images/slide2.jpg',
+    imageBg: '/images/slide2b.webp',
+    title: 'Suits'
+  },
+  {
+    id: 3,
+    image: '/images/slide3.jpg',
+    imageBg: '/images/slide3b.webp',
+    title: 'Tales By Light',
+  },
+  {
+    id: 4,
+    image: '/images/slide4.jpg',
+    imageBg: '/images/slide4b.webp',
+    title: 'Futsal'
+  },
+  {
+    id: 5,
+    image: '/images/slide5.jpg',
+    imageBg: '/images/slide5b.webp',
+    title: 'Control Cricket'
+  },
+  {
+    id: 6,
+    image: '/images/slide5.jpg',
+    imageBg: '/images/slide5b.webp',
+    title: 'Badminton'
+  }
+];
+
+const basic = [
+  {
+    id: 1,
+    image: '/images/slide1.jpg',
+    imageBg: '/images/slide1b.webp',
+    title: 'Brahman Naman'
+  },
+  {
+    id: 2,
+    image: '/images/slide2.jpg',
+    imageBg: '/images/slide2b.webp',
+    title: 'Glow Up'
+  },
+  {
+    id: 3,
+    image: '/images/slide3.jpg',
+    imageBg: '/images/slide3b.webp',
+    title: 'American Name',
+  },
+  {
+    id: 4,
+    image: '/images/slide4.jpg',
+    imageBg: '/images/slide4b.webp',
+    title: 'Table Tennis'
+  },
+  {
+    id: 5,
+    image: '/images/slide5.jpg',
+    imageBg: '/images/slide5b.webp',
+    title: 'PUBG'
+  },
+  {
+    id: 6,
+    image: '/images/slide5.jpg',
+    imageBg: '/images/slide5b.webp',
+    title: 'FIFA'
+  },
+  {
+    id: 7,
+    image: '/images/slide5.jpg',
+    imageBg: '/images/slide5b.webp',
+    title: 'Carrom'
   }
 ];
 
@@ -45,8 +162,31 @@ class App extends Component {
   render() {
     return (
       <div className="app">
+        
+        <h2>Elite</h2>
         <Slider>
-          {movies.map(movie => (
+          {elite.map(movie => (
+            <Slider.Item movie={movie} key={movie.id}>item1</Slider.Item>
+          ))}
+        </Slider>
+
+        <h2>Premium</h2>
+        <Slider>
+          {premium.map(movie => (
+            <Slider.Item movie={movie} key={movie.id}>item1</Slider.Item>
+          ))}
+        </Slider>
+
+        <h2>Standard</h2>
+        <Slider>
+          {standard.map(movie => (
+            <Slider.Item movie={movie} key={movie.id}>item1</Slider.Item>
+          ))}
+        </Slider>
+
+        <h2>Basic</h2>
+        <Slider>
+          {basic.map(movie => (
             <Slider.Item movie={movie} key={movie.id}>item1</Slider.Item>
           ))}
         </Slider>
